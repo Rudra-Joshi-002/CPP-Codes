743174fdfdc162b9b3d7f1e194981dbda636a8e4
Rudra_Joshi <164770297+Rudra-Joshi-002@users.noreply.github.com>
2024-05-06 10:17:31 +0530

    Q-3.cpp

--- Apna College C++ DSA Code/Conditional Statements Assignment Question Solution/Q-3.cpp
+++ Apna College C++ DSA Code/Conditional Statements Assignment Question Solution/Q-3.cpp
@@ -1,4 +1,4 @@
-/*Question 5 : For any 3 digit number check whether it’s an Armstrong number or not.
+/*Question 3 : For any 3 digit number check whether itâ€™s an Armstrong number or not.
 Armstrong number is a number that is equal to the sum of cubes of its digits.
 Eg : 371 is an armstrong number.
 3*3*3 + 7*7*7 + 1*1*1 = 371*/
