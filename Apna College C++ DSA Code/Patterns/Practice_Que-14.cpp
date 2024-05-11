4367127c51411bbfc6253c34b09d17ac7c48de01
Rudra_Joshi <164770297+Rudra-Joshi-002@users.noreply.github.com>
2024-05-11 17:29:18 +0530

    Add files via upload

---
+++ Apna College C++ DSA Code/Patterns/Practice_Que-14.cpp
@@ -0,0 +1,70 @@
+/* Print The Following Pattern Using Loops:
+
+*             *
+* *         * *
+* * *     * * *
+* * * * * * * *
+* * * * * * * *
+* * *     * * *
+* *         * *
+*             *
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout<<"Enter a Integer Number: ";
+
+    cin>>num;
+
+    for(int i=1;i<num+1;i++)
+    {
+
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+
+        for(int j=1;j<=2*(num-i);j++)
+        {
+            cout<<"  ";
+        }
+
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+
+        cout<<endl;
+
+    }
+
+    for(int i=num;i>0;i--)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+
+        for(int j=1;j<=2*(num-i);j++)
+        {
+            cout<<"  ";
+        }
+
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+
+}
